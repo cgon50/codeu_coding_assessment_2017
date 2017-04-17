a5bda8dd4d2dcb6298ddb692a432e9a326dea9d7
Christian Gonzalez <cgonzalezred@utexas.edu>
2017-04-17 11:24:49 -0500

    Update MyJSONParser.java

--- src/com/google/codeu/codingchallenge/MyJSONParser.java
+++ src/com/google/codeu/codingchallenge/MyJSONParser.java
@@ -15,12 +15,80 @@
 package com.google.codeu.codingchallenge;
 
 import java.io.IOException;
+import java.util.Scanner;
+import java.util.HashMap;
+import java.util.Map;
 
 final class MyJSONParser implements JSONParser {
 
-  @Override
-  public JSON parse(String in) throws IOException {
-    // TODO: implement this
-    return new MyJSON();
-  }
+
+	@Override
+	public JSON parse(String in) throws IOException {
+		//count brackets make sure they match
+		if(!bracketsOkay(in)){
+			throw new illegalStateError("Mismatched brackets error");
+		}
+		//skip until next nonbrace - non space character
+		int start = nonSpaceIndex(in, 0);
+		//if the first charater isnt a quotation mark then bad syntax
+		if(in.charAt(start) == '\\'){
+			
+		}
+		if(!in.charAt(start) == "\""){
+			 throw new illegalStateError("mismatched quotation mark");
+		}
+		//go past the quotation mark
+		start = start + 1;
+		String key = getKey(in, start);
+		return new MyJSON();
+	}
+	private String escapeCharacters(){
+		
+	}
+	private String getKey(String in, int index){
+		StringBuilder key = new StringBuilder();
+		for(int i = index; i < in.length(); i++){
+			if(in.charAt(i) == "\""){
+				break;
+			}else{
+				key.append(in.charAt(i));
+			}
+		}
+		return key.toString();
+	}
+	//finds the next part of the string that is a non brace and non space
+	private int nonSpaceIndex(String in, int index){
+		for(int i = index; i < in.length(); i++){
+			char current = in.charAt(i);
+			if(current != '{' && current != ')' && current != ' ' && current != '}' 
+					&& current != '(' ){
+				return i;
+			}
+		}
+		//if no more non space/nonbracket characters
+		return -1;
+	}
+
+	//helper method to skip any elemets equal to whatever is passed in
+	private void skipElementsEqualToX(Scanner sc, String x){
+		String element = sc.next();
+		while(element.equals(x)){
+			element = sc.next();
+		}
+	}
+	//checks that all objects have matching brackets
+	private boolean bracketsOkay(String in){
+		int numOpenBraces = 0;
+		int numCloseBraces = 0;
+
+		for(int i = 0; i < in.length(); i++){
+			char currentChar = in.charAt(i);
+			if(currentChar == '{'){
+				openBraces++;
+			}else if(currentChar == '}'){
+				closeBraces++;
+			}
+		}
+		return numOpenBraces == numCloseBraces;
+	}
 }
